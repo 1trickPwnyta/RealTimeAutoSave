272d530e96c020a1ba1d24f4fba8cc718cd4fe3d
1trickPonyta <1trickPonyta@kangaroostandard.com>
2021-07-17 10:17:24 -0700

    Initial commit. Prevent autosave entirely.

---
+++ Source/Mod.cs
@@ -0,0 +1,18 @@
+﻿using Verse;
+using HarmonyLib;
+
+namespace RealTimeAutoSave
+{
+    [StaticConstructorOnStartup]
+    public static class Mod
+    {
+        const string PACKAGE_ID = "realtimeautosave.1trickPonyta";
+        static Mod()
+        {
+            var harmony = new Harmony(PACKAGE_ID);
+            harmony.PatchAll();
+
+            Log.Message("[Real Time Auto Save] Loaded.");
+        }
+    }
+}
---
+++ Source/Patch_Autosaver.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using HarmonyLib;
+
+namespace RealTimeAutoSave
+{
+    [HarmonyPatch(typeof(Autosaver))]
+    [HarmonyPatch("AutosaverTick")]
+    public class Patch_Autosaver
+    {
+        static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
