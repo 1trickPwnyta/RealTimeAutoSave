4830955dd621fb33264a3c87fa9f5cc1169a01d2
1trickPonyta <1trickPonyta@kangaroostandard.com>
2021-07-22 08:30:54 -0700

    Update deploy script to be multi version friendly

--- Deploy.ps1
+++ Deploy.ps1
@@ -4,15 +4,22 @@ Copy-Item "$path\About\PublishedFileId.txt" ..\About
 Remove-Item -Recurse "$path\*"
 mkdir $path
 @(
-	"About",
-	"Assemblies",
-	"Defs",
-	"Languages",
-	"Patches",
-	"Source",
-	"Textures"
-) | %{ Copy-Item -Recurse "..\$_" "$path\$_" }
-Remove-Item -Recurse "$path\Source\bin"
-Remove-Item -Recurse "$path\Source\obj"
-Remove-Item "$path\Source\packages.config"
-Remove-Item ..\About\PublishedFileId.txt
+	".",
+	"1.2",
+	"1.3"
+) | %{
+	$base = $_
+	@(
+		"About",
+		"Assemblies",
+		"Defs",
+		"Languages",
+		"Patches",
+		"Source",
+		"Textures"
+	) | %{ Copy-Item -Recurse "$PSScriptRoot\$base\$_" "$path\$base\$_" }
+	Remove-Item -Recurse "$path\$base\Source\bin"
+	Remove-Item -Recurse "$path\$base\Source\obj"
+	Remove-Item "$path\$base\Source\packages.config"
+	Remove-Item "$path\$base\\About\PublishedFileId.txt"
+}
