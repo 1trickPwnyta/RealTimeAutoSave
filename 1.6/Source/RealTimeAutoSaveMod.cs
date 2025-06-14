efa8ee2c819818df5e47a60fd69dcf09ac3c3731
1trickPwnyta <1trickPwnyta@kangaroostandard.com>
2025-06-14 09:10:59 -0700

    Update to 1.6

---
+++ 1.6/Source/RealTimeAutoSaveMod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using HarmonyLib;
+
+namespace RealTimeAutoSave
+{
+    public class RealTimeAutoSaveMod : Mod
+    {
+        public const string PACKAGE_ID = "realtimeautosave.1trickPonyta";
+        public const string PACKAGE_NAME = "Real-Time Auto Save";
+
+        public static RealTimeAutoSaveSettings Settings;
+
+        public RealTimeAutoSaveMod(ModContentPack content) : base(content)
+        {
+            Settings = GetSettings<RealTimeAutoSaveSettings>();
+
+            var harmony = new Harmony(PACKAGE_ID);
+            harmony.PatchAll();
+
+            Log.Message($"[{PACKAGE_NAME}] Loaded.");
+
+            // If using a real-time interval, start the timer on load
+            if (RealTimeAutoSaveSettings.AutoSaveMode == AutoSaveMode.RealTime)
+            {
+                RealTimeAutosaver.ScheduleAutosave();
+            }
+        }
+
+        public override string SettingsCategory() => PACKAGE_NAME;
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            RealTimeAutoSaveSettings.DoSettingsWindowContents(inRect);
+        }
+    }
+}
---
+++ 1.6/Source/RealTimeAutoSaveSettings.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace RealTimeAutoSave
+{
+    public enum AutoSaveMode
+    {
+        InGame,     // In-game days
+        RealTime,   // Real-time minutes
+        None
+    }
+
+    public class RealTimeAutoSaveSettings : ModSettings
+    {
+        public static AutoSaveMode AutoSaveMode = AutoSaveMode.RealTime;
+        public static float InGameInterval = 1f;
+        public static int RealTimeInterval = 10;
+
+        public static void DoSettingsWindowContents(Rect inRect)
+        {
+            string buffer;
+
+            Listing_Standard listingStandard = new Listing_Standard();
+
+            listingStandard.Begin(inRect);
+            listingStandard.Label("RealTimeAutoSave_SettingsAutosaveInterval".Translate());
+
+            if (listingStandard.RadioButton("RealTimeAutoSave_SettingsInGameInterval".Translate(), AutoSaveMode == AutoSaveMode.InGame))
+            {
+                AutoSaveMode = AutoSaveMode.InGame;
+                RealTimeAutosaver.CancelAutosave();
+            }
+            if (AutoSaveMode == AutoSaveMode.InGame)
+            {
+                // Show the vanilla-ish autosave interval selection drop down, but with one difference: 
+                // we gives ours a (fake) title of "" (empty string), which has no UI effect but allows 
+                // us to differentiate this drop down from the actual vanilla drop down when we patch
+                // out the vanilla one
+                buffer = InGameInterval.ToString();
+                string days = "Days".Translate();
+                string day = "Day".Translate().ToLower();
+                if (listingStandard.ButtonText($"{InGameInterval} {(InGameInterval == 1f ? day : days)}"))
+                {
+                    List<FloatMenuOption> list = new List<FloatMenuOption>();
+                    float[] debugChoices = new float[] { 0.125f, 0.25f };
+                    float[] choices = new float[] { 0.5f, 1f, 3f, 7f, 14f };
+                    if (Prefs.DevMode)
+                    {
+                        foreach (float choice in debugChoices)
+                        {
+                            list.Add(new FloatMenuOption($"{choice} {days} (debug)", delegate ()
+                            {
+                                InGameInterval = choice;
+                            }, MenuOptionPriority.Default, null, null, 0f, null, null));
+                        }
+                    }
+                    foreach (float choice in choices)
+                    {
+                        list.Add(new FloatMenuOption($"{choice} {(choice == 1f? day : days)}", delegate ()
+                        {
+                            InGameInterval = choice;
+                        }, MenuOptionPriority.Default, null, null, 0f, null, null));
+                    }
+                    Find.WindowStack.Add(new FloatMenu(list, ""));
+                }
+            }
+
+            if (listingStandard.RadioButton("RealTimeAutoSave_SettingsRealTimeInterval".Translate(), AutoSaveMode == AutoSaveMode.RealTime))
+            {
+                AutoSaveMode = AutoSaveMode.RealTime;
+                RealTimeAutosaver.ScheduleAutosave();
+            }
+            if (AutoSaveMode == AutoSaveMode.RealTime)
+            {
+                // Show the real-time interval input box
+                buffer = RealTimeInterval.ToString();
+                listingStandard.IntEntry(ref RealTimeInterval, ref buffer);
+                RealTimeInterval = Math.Max(RealTimeInterval, 1);
+                RealTimeAutosaver.ScheduleAutosave();
+            }
+
+            if (listingStandard.RadioButton("RealTimeAutoSave_SettingsNone".Translate(), AutoSaveMode == AutoSaveMode.None))
+            {
+                AutoSaveMode = AutoSaveMode.None;
+                RealTimeAutosaver.CancelAutosave();
+            }
+
+            listingStandard.End();
+        }
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref AutoSaveMode, "AutoSaveMode");
+            Scribe_Values.Look(ref InGameInterval, "InGameInterval");
+            Scribe_Values.Look(ref RealTimeInterval, "RealTimeInterval");
+        }
+    }
+
+    // Window that is shown when clicking the patched-in Configure button in the options 
+    // dialog. It just mimics the mod settings dialog.
+    public class RealTimeAutoSaveSettingsDialog : Window
+    {
+        public RealTimeAutoSaveSettingsDialog()
+        {
+            this.forcePause = true;
+            this.doCloseX = true;
+            this.doCloseButton = true;
+            this.closeOnClickedOutside = true;
+            this.absorbInputAroundWindow = true;
+        }
+
+        public override void DoWindowContents(Rect inRect)
+        {
+            RealTimeAutoSaveSettings.DoSettingsWindowContents(inRect);
+        }
+
+        public override void PostClose()
+        {
+            base.PostClose();
+            RealTimeAutoSaveMod.Settings.Write();
+        }
+    }
+}
